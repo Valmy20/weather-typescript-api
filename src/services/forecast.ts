bd496248f757f7be865b1d22e4fcd93b3b41f349
valmy <valmyericles@gmail.com>
2020-08-31 12:06:48 -0300

    agrupando forecasts por hora

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,12 +15,19 @@ export interface Beach {
   user: string;
 }
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[]
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
-  public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<TimeForecast[]> {
     
     const pointsWithCorrectSources: BeachForecast[] = [];
 
@@ -44,6 +51,24 @@ export class Forecast {
 
     }
 
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+
+    for(const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if(timePoint) {
+        timePoint.forecast.push(point)
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point]
+        })
+      }
+    }
+
+    return forecastByTime;
   }
 }
